202f9e44918853a05ad77af4764fa0036f5223ce
Madhav Madhav <madhav.sharma@idemo-ppc.com>
2017-02-24 18:01:21 +0530

    Final commit for the day

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,16 @@
 var db = require('../database/database');
 
+//Documentation avaible at npm bcryptjs
+// Responsible for encrypting the database
+var bcrypt = require('bcryptjs');
+//Salt will hash the password 5 times. 
+var salt = bcrypt.genSaltSync(5);
+
 module.exports.createUser = function (req, res) {
-    var query = "INSERT INTO users (username, user_password, email) VALUES ('" + req.body.username + "', '" + req.body.user_password + "', '" + req.body.email + "')";   
-    
+    //sync the current PW with the hash
+    var password = bcrypt.hashSync(req.body.user_password, salt);
+    var query = "INSERT INTO users (username, user_password, email) VALUES ('" + req.body.username + "', '" + password + "', '" + req.body.email + "')";
+
     // Firing the query I just created
     db.query(query).spread(function (result, metadata) {
         // If successful, send 200 status and the message
