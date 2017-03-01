e4ef478662a181181f07d385635fee89be3a8dfa
Madhav Madhav <madhav.sharma@idemo-ppc.com>
2017-03-01 08:51:37 +0530

    more additions

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,8 +8,23 @@ router.use(bodyParser.urlencoded({
     extended: true
 }));
 
+//GET ENDPOINTS
+
+router.get('/get_freinds', function (req, res) {
+    var query = "SELECT * FROM user_friends WHERE user_id = " + req.body.user_id;
+
+    db.query(query).spread(function (result, metadata) {
+        res.json({
+            data: result
+        });
+    }).catch(function (err) {
+        res.status(500).send(err);
+    })
+})
+
+//POST ENDPOINTS
 router.post('/request_friend', function (req, res) {
-    //Check to see if the request has already been sent Or if they are already friends
+    //Check to see if a request has already been sent... OR if they are already friends
     var query = "SELECT * FROM user_friend_requests WHERE sender_id=" + req.body.sender_id + " AND received_id=" + req.body.received_id;
 
     db.query(query).spread(function (result, metadata) {
@@ -17,18 +32,76 @@ router.post('/request_friend', function (req, res) {
             insertRequest();
         }
     }).catch(function (err) {
-        res.status(500).send(err)
+        res.status(500).send(err);
     });
 
     function insertRequest() {
-        var query = "INSERT INTO user_friend_requests (sender_id, reciever_id, status) VALUES sender_id=" + req.body.sender_id + ", AND received_id=" + req.body.received_id + " status = 'pending'";
+        var query = "INSERT INTO user_friend_requests (sender_id, received_id, status) VALUES(" + req.body.sender_id + "," + req.body.received_id + ",'pending')";
 
         db.query(query).spread(function (result, metadata) {
-            res.status(200).send('Friend Request Created Successfully');
-        }).catch(function () {
-            res.status(500).send(err);
+            res.status(200).send("Friend Request Created Successfully.")
+        }).catch(function (err) {
+            res.status(500).send(err)
         })
     }
+});
+router.post('/request_friend_respond', function (req, res) {
+    //Check To See if the Request even exists
+    var query = "SELECT * FROM user_friend_requests WHERE id=" + req.body.request_id;
+    var senderId;
+    var receivedId;
+
+    db.query(query).spread(function (result, metadata) {
+        if (result.length > 0) {
+            senderId = result[0].sender_id;
+            receivedId = result[0].received_id;
+            updateRequest();
+        } else {
+            res.status(400).send("Request Doesn't Exist :(")
+        }
+    });
+
+    function updateRequest() {
+        var isAccepted = req.body.confirmation === 'confirmed';
+        var query;
+
+        if (isAccepted) {
+            query = "UPDATE user_friend_requests SET status='confirmed' WHERE id=" + req.body.request_id;
+        } else {
+            query = "DELETE FROM user_friend_requests WHERE id=" + req.body.request_id;
+        }
+
+        db.query(query).spread(function () {
+            if (isAccepted) {
+                performSenderInsert();
+            } else {
+                res.status(200).send("We have successfully deleted the request.")
+            }
+        }).catch(function () {
+            res.status(400).send("Unable to process Update to User_Friend_Requests at this time")
+        })
+    }
+
+    function performSenderInsert() {
+        var query = "INSERT INTO user_friends (user_id, friend_id, date_friended) VALUES (" + senderId + ", " + receivedId + ", now())";
+
+        db.query(query).spread(function () {
+            performReceiverInsert();
+        }).catch(function () {
+            res.status(500).send("Unable to send a friend request at this time.")
+        })
+    }
+
+    function performReceiverInsert() {
+        var query = "INSERT INTO user_friends (user_id, friend_id, date_friended) VALUES (" + receivedId + ", " + senderId + ", now())";
+
+        db.query(query).spread(function () {
+            res.status(200).send("The user was successfully confirmed")
+        }).catch(function () {
+            res.status(500).send("Unable to send a friend request at this time.")
+        })
+    }
+
 });
 
 module.exports = router;
